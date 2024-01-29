16a4529ca4f9a3a5fc6f3b090565d1ac7c20d2e6
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2024-01-29 14:58:28 +0100

    chore : remake migrations & re init project

--- src/DataFixtures/NationaliteFixture.php
+++ src/DataFixtures/NationaliteFixture.php
@@ -1,1 +1,28 @@
 <?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Nationalite;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class NationaliteFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // Création d'un tableau de nationalités fictives
+        $nationalites = ['Française', 'Américaine', 'Britannique', 'Canadienne', 'Australienne'];
+
+        foreach ($nationalites as $i => $nomNationalite) {
+            $nationalite = new Nationalite();
+            $nationalite->setNationalite($nomNationalite);
+
+            $manager->persist($nationalite);
+
+            // Ajouter une référence pour être utilisée dans d'autres fixtures
+            $this->addReference('nationalite_' . ($i + 1), $nationalite);
+        }
+
+        $manager->flush();
+    }
+}
